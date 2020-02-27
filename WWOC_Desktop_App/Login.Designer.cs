a296c174e73ae380ddd9f0dcf505ba9aa3ff88be
Robert Duff <rduff154@gmail.com>
2020-02-27 14:10:32 -0600

    Testing window resizing

--- WWOC_Desktop_App/Login.Designer.cs
+++ WWOC_Desktop_App/Login.Designer.cs
@@ -33,70 +33,78 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(245, 42);
+            this.textBox1.Location = new System.Drawing.Point(163, 27);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 26);
+            this.textBox1.Size = new System.Drawing.Size(99, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(245, 104);
+            this.textBox2.Location = new System.Drawing.Point(163, 68);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 26);
+            this.textBox2.Size = new System.Drawing.Size(99, 20);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(124, 53);
+            this.label1.Location = new System.Drawing.Point(83, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(124, 110);
+            this.label2.Location = new System.Drawing.Point(83, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "password";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 12);
+            this.label3.Location = new System.Drawing.Point(77, 8);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(229, 20);
+            this.label3.Size = new System.Drawing.Size(155, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "really terrible initial login window";
             // 
-            // button1
+            // btnLogin
             // 
-            this.button1.Location = new System.Drawing.Point(125, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 80);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogin.Location = new System.Drawing.Point(83, 105);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(135, 52);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 285);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(342, 185);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -111,7 +119,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- WWOC_Desktop_App/Login.cs
+++ WWOC_Desktop_App/Login.cs
@@ -10,11 +10,22 @@ using System.Windows.Forms;
 
 namespace WWOC_Desktop_App
 {
+    //to use panels or tab control for multiple form selection.
     public partial class Login : Form
     {
+        private int originalWidth;
+        private int originalHeight;
         public Login()
         {
             InitializeComponent();
+            originalWidth = this.Width;
+            originalHeight = this.Height;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            this.Width = 1000;
+            this.Height = 1000;
         }
     }
 }
