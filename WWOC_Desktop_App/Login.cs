8a8f7f499b61377ad87159c2dd051113aee9fc76
Robert Duff <rduff154@gmail.com>
2020-03-13 20:07:44 -0500

    login functionality semi works

--- WWOC_Desktop_App/Login.cs
+++ WWOC_Desktop_App/Login.cs
@@ -26,18 +26,70 @@ namespace WWOC_Desktop_App
             using (SqlConnection cnn = new SqlConnection("Data Source=10.135.85.184;Initial Catalog=GROUP4;User ID=Group4;Password=Grp4s2117"))
             {
                 cnn.Open();
-                string checkUser = "EXEC checkUsername @Username =" + tbUsername.Text + ", @Password =" + tbPassword.Text + ";";
+                //string checkUser = "EXEC checkUsername @Username =" + tbUsername.Text + ", @Password =" + tbPassword.Text + ";";
+                string checkUser = "EXEC isUsername @Username =" + tbUsername.Text + ";";
                 SqlCommand sqlComm = new SqlCommand(checkUser, cnn);
                 try
                 {
                     SqlDataReader reader = sqlComm.ExecuteReader();
-                    if (reader.HasRows == true)
+                    if (reader.HasRows == true)//username is in the database
                     {
-                        MessageBox.Show("account is a thing");
+                        user user = new user();
+                        reader.Read();
+                        user.username = reader["username"].ToString();
+                        //reader.Close();
+                        //Account Exists time to check the password
+                        string checkPass = "EXEC checkUsername @Username =" + tbUsername.Text + ", @Password =" + tbPassword.Text + ";";
+                        sqlComm = new SqlCommand(checkPass, cnn);
+                        reader = sqlComm.ExecuteReader();
+                        if(reader.HasRows == true)
+                        {
+                            reader.Read();
+                            string pullUser = "EXEC PullUser @UserId =" + reader["userID"] + ";";
+                            //reader.Close(); //password matches
+                            sqlComm = new SqlCommand(pullUser, cnn);
+                            reader = sqlComm.ExecuteReader();
+                            reader.Read();
+                            //saves info from database to new user object
+                            user.UserID = Convert.ToInt32(reader["userID"]);
+                            user.username = reader["username"].ToString();
+                            user.password = reader["password"].ToString();
+                            user.name = reader["name"].ToString();
+                            user.accessLevel = Convert.ToInt32(reader["accesslevel"]);
+                            user.lockedOut = Convert.ToInt32(reader["lockedOut"]);
+                            user.counter = 0;
+                            //some security checks
+                            if (user.lockedOut == 1)//checks if user is already locked out
+                            {
+                                MessageBox.Show("Account is locked");
+                            }
+                            else if (user.counter >= 3)//checks if 3rd attempt to sign in has been made
+                            {
+                                MessageBox.Show("3 failed attempts account is now locked");
+                                user.lockedOut = 1;
+                            }
+                            else
+                            {
+                                Form MainMenu = new MainMenu();
+                                MainMenu.Show();
+                                this.Hide();
+                            }
+                        }
+                        else
+                        {
+                            string pullCounter = "EXEC pullCounter @Username =" + user.username + ";";
+                            sqlComm = new SqlCommand(pullCounter, cnn);
+                            reader = sqlComm.ExecuteReader();
+                        }
+                        
+
+                        
+
+                        user.updateDatabase();//updates the database after possible adjustments have been made
                     }
                     else
                     {
-                        MessageBox.Show("Not a thing");
+                        MessageBox.Show("Account does not exist");
                     }
                 }
                 catch(Exception ex)
---
+++ WWOC_Desktop_App/user.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+
+namespace WWOC_Desktop_App
+{
+    class user
+    {
+        public int UserID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public int accessLevel { get; set; }
+        public int lockedOut { get; set; }
+        public int counter { get; set; }
+
+        /*Updates the database in the event that the accessLevel,lockedout, or counter is changed during runtime.
+         * Edit stored procedure "UpdateUsers" if more features should be added.
+         * Returns: nothing
+         */
+        public void updateDatabase()
+        {
+            using (SqlConnection cnn = new SqlConnection("Data Source=10.135.85.184;Initial Catalog=GROUP4;User ID=Group4;Password=Grp4s2117"))
+            {
+                cnn.Open();
+                SqlCommand sqlComm = new SqlCommand(("EXEC UpdateUsers @UserId =" + UserID + ", @AccessLevel =" + accessLevel + ", @LockedOut =" + lockedOut + ", @Counter =" + counter + ";"), cnn);
+                sqlComm.ExecuteNonQuery();
+                cnn.Close();
+            }
+        }//end updateDatabase
+
+    }//end user
+}
